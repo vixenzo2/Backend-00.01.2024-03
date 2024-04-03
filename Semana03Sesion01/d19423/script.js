eba8482ac538800b92bde75946d76b77fa54a86b
Roberto Pineda <rpineda@x-codec.net>
2024-04-02 22:33:57 -0500

    clase 02042024 segunda parte parte

--- Semana03Sesion01/d19423/script.js
+++ Semana03Sesion01/d19423/script.js
@@ -1,4 +1,50 @@
-let boton1 = document.getElementById("btnTercerBoton");
-        boton1.addEventListener("click",function(){
-            alert("Hola desde el tercer boton")
-        });
+// let boton1 = document.getElementById("btnTercerBoton"); 
+// boton1.addEventListener("click", function () { 
+//     alert("Hola desde el tercer boton") 
+// });
+
+
+// var edad = 40;
+
+
+// var edad = 55;
+
+let edad = 40;
+//let edad = 55;
+{
+    let edad = 55;
+    console.log(edad);
+}
+
+console.log(edad);
+
+const PI = 3.14159;
+
+let SaludoDeMiMadre = "Hola hijitos";
+let saludo_de_mi_padre = "Hola chicos";
+//let saludo-de-mi-padre = "Hola chicos";
+
+//PI=4.5;
+
+// Tipos de datos numerico
+let entero = 999999999;
+let decimal = 22.99;
+let notacionCientifica = 123e5; 
+
+// let x = 10;
+// let y = 20;
+// let z = x + y;
+// let x = 10;
+// let y = "20";
+// let z = x + y;
+
+let x = 100 / "Apple";
+console.log(isNaN(x));
+
+// Tipos de datos de cadena de texto
+
+// Tipo de datos de fechas
+
+// Tipos de datos booleanos
+// OR  AND 
+
