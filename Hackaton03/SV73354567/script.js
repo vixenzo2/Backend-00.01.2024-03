57af0490f0ad9b4fd166d73d32c305002aa3fb65
Kevin Tenaoza <keviintc96@gmail.com>
2024-04-05 17:42:59 -0500

    Avance1al2

--- Hackaton03/SV73354567/script.js
+++ Hackaton03/SV73354567/script.js
@@ -1,21 +1,131 @@
 function ejecutar(nroEjercicio) {
     switch (nroEjercicio) {
         case 1:
-            ejecicio1();
+            ejercicio1();
             break;
         case 2:
-            ejecicio2();
+            ejercicio2();
             break;
-            case 3:
-                ejecicio2();
-                break;
+        case 3:
+            ejercicio3();
+            break;
+        case 4:
+            ejercicio4();
+            break;
+        case 5:
+            ejercicio5();
+            break;
+        case 6:
+            ejercicio6();
+            break;
+        case 7:
+            ejercicio7();
+            break;
+        case 8:
+            ejercicio8();
+            break;
+        case 9:
+            ejercicio9();
+            break;
+        case 10:
+            ejercicio10();
+            break;
+        case 11:
+            ejercicio11();
+            break;
+        case 12:
+            ejercicio12();
+            break;
+        case 13:
+            ejercicio13();
+            break;
+        case 14:
+            ejercicio14();
+            break;
+        case 15:
+            ejercicio15();
+            break;
+        case 16:
+            ejercicio16();
+            break;
+        case 17:
+            ejercicio17();
+            break;
+        case 18:
+            ejercicio18();
+            break;
+        case 19:
+            ejercicio19();
+            break;
+        case 20:
+            ejercicio20();
+            break;
+        case 21:
+            ejercicio21();
+            break;
+        case 22:
+            ejercicio22();
+            break;
+        case 23:
+            ejercicio23();
+            break;
+        case 24:
+            ejercicio24();
+            break;
+        case 25:
+            ejercicio25();
+            break;
+        case 26:
+            ejercicio26();
+            break;
+        case 27:
+            ejercicio27();
+            break;
+        case 28:
+            ejercicio28();
+            break;
+        case 29:
+            ejercicio29();
+            break;
+        case 30:
+            ejercicio30();
+            break;
+        case 31:
+            ejercicio31();
+            break;
+        case 32:
+            ejercicio32();
+            break;
+        case 33:
+            ejercicio33();
+            break;
+        case 34:
+            ejercicio34();
+            break;
+        case 35:
+            ejercicio35();
+            break;
+        case 36:
+            ejercicio36();
+            break;
+        case 37:
+            ejercicio37();
+            break;
+        case 38:
+            ejercicio38();
+            break;
+        case 39:
+            ejercicio39();
+            break;
+        case 40:
+            ejercicio40();
+            break;
         default:
             break;
     }
-
 }
 
-function ejecicio1() {
+function ejercicio1() {
     let numero = Number.parseInt(prompt("Escribe tu numero"));
     console.log(numero)
     if (!isNaN(numero)) {
@@ -26,13 +136,174 @@ function ejecicio1() {
             alert("No tiene 3 digitos");
         }
     }
-    else{
+    else {
         alert("Lo que ingreso no son numeros");
     }
     console.log("Ejercicio 1")
 }
 
-function ejecicio2() {
+function ejercicio2() {
+    let numero = Number.parseInt(prompt("Por favor, ingresa un número entero:"));
+    if (!isNaN(numero)) {
+        if (numero < 0) {
+            alert("El número ingresado es negativo.");
+        }
+        else if (numero === 0) {
+            alert("El número ingresado es cero, no es positivo ni negativo.");
+        }
+        else {
+            alert("El número ingresado es positivo.");
+        }
+    }
+    else {
+        alert("Lo que ingreso no son numeros");
+    }
     console.log("Ejercicio 2")
 }
 
+
+
+
+
+function ejercicio3() {
+    console.log("Ejercicio 3")
+}
+function ejercicio4() {
+    console.log("Ejercicio 4")
+}
+function ejercicio5() {
+    console.log("Ejercicio 5")
+}
+function ejercicio6() {
+    console.log("Ejercicio 6")
+}
+function ejercicio7() {
+    console.log("Ejercicio 7")
+}
+
+function ejercicio8() {
+    console.log("Ejercicio 8");
+}
+
+function ejercicio9() {
+    console.log("Ejercicio 9");
+}
+function ejercicio10() {
+    console.log("Ejercicio 10");
+}
+
+function ejercicio11() {
+    console.log("Ejercicio 11");
+}
+function ejercicio12() {
+    console.log("Ejercicio 12");
+}
+
+function ejercicio13() {
+    console.log("Ejercicio 13");
+}
+
+function ejercicio14() {
+    console.log("Ejercicio 14");
+}
+function ejercicio15() {
+    console.log("Ejercicio 15");
+}
+
+function ejercicio16() {
+    console.log("Ejercicio 16");
+}
+function ejercicio17() {
+    console.log("Ejercicio 17");
+}
+
+function ejercicio18() {
+    console.log("Ejercicio 18");
+}
+
+function ejercicio19() {
+    console.log("Ejercicio 19");
+}
+
+function ejercicio20() {
+    console.log("Ejercicio 20");
+}
+
+function ejercicio21() {
+    console.log("Ejercicio 21");
+}
+
+function ejercicio22() {
+    console.log("Ejercicio 22");
+}
+
+function ejercicio23() {
+    console.log("Ejercicio 23");
+}
+function ejercicio24() {
+    console.log("Ejercicio 24");
+}
+
+function ejercicio25() {
+    console.log("Ejercicio 25");
+}
+
+function ejercicio26() {
+    console.log("Ejercicio 26");
+}
+
+function ejercicio27() {
+    console.log("Ejercicio 27");
+}
+
+function ejercicio28() {
+    console.log("Ejercicio 28");
+}
+
+function ejercicio29() {
+    console.log("Ejercicio 29");
+}
+
+function ejercicio30() {
+    console.log("Ejercicio 30");
+}
+
+function ejercicio31() {
+    console.log("Ejercicio 31");
+}
+
+function ejercicio32() {
+    console.log("Ejercicio 32");
+}
+
+function ejercicio33() {
+    console.log("Ejercicio 33");
+}
+
+function ejercicio34() {
+    console.log("Ejercicio 34");
+}
+
+function ejercicio35() {
+    console.log("Ejercicio 35");
+}
+
+function ejercicio36() {
+    console.log("Ejercicio 36");
+}
+
+function ejercicio37() {
+    console.log("Ejercicio 37");
+}
+
+function ejercicio38() {
+    console.log("Ejercicio 38");
+}
+
+function ejercicio39() {
+    console.log("Ejercicio 39");
+}
+
+function ejercicio40() {
+    console.log("Ejercicio 40");
+}
